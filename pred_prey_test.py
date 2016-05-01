055f4bfd2ac41a3c0bbd3b74a05eae3886cfac67
hadleigh <hadleighthompson@gmail.com>
2016-05-01 08:35:00 -0700

    first commit

---
+++ pred_prey_test.py
@@ -0,0 +1,19 @@
+"""
+pre_prey_test.py
+
+ENVR 300
+
+H Thompson
+
+March 2016
+"""
+
+import numpy as np
+import yaml
+import pandas as pd
+import matplotlib.pyplot as plt
+
+v_old = 1.
+p_old = 3.2
+
+y_initial = [v_old, p
